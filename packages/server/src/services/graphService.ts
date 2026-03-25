819d3a41138cdb46abbba8b0f6feec95fded0bb1
Pascal Watteel <pascal@watteel.com>
2026-03-25 13:54:50 +0400

    fix(server): remove unused hasAccess import in graphService

--- packages/server/src/services/graphService.ts
+++ packages/server/src/services/graphService.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../prisma.js";
-import { hasAccess, getAccessibleSharedPaths } from "./shareService.js";
+import { getAccessibleSharedPaths } from "./shareService.js";
 
 const WIKI_LINK_REGEX = /\[\[([^\]]+)\]\]/g;
 
